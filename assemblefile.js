8ebd889968f7b5574422db40d327c6da414e1b81
Brian Woodward <brian.woodward@gmail.com>
2015-09-18 17:03:54 -0400

    loading data/partials/layouts and some helpers

--- assemblefile.js
+++ assemblefile.js
@@ -1,14 +1,25 @@
 'use strict';
 
+var loader = require('assemble-loader');
+var extname = require('gulp-extname');
 var assemble = require('assemble');
+var Plasma = require('plasma');
+var path = require('path');
 
-var app = assemble();
-app.option('archive.version', '0.4.0');
+
+var plasma = new Plasma();
+var app = assemble()
+  .use(loader())
+
+app.option('archive.version', '0.6.0');
+app.option('rethrow', false);
+app.option('renameKey', function (fp) {
+  return path.basename(fp, path.extname(fp));
+});
 
 // archive a _gh_pages folder to the specific version folder and build a manifest of archived files
 var archive = require('./src/plugins/archive')(app);
 app.task('archive', archive(['_gh_pages/**/*', '!_gh_pages/v/**/*', '!_gh_pages/v'], 'versions'));
-
 
 // copy archived versions to the docs/v folder
 app.task('versions', function () {
@@ -16,6 +27,24 @@ app.task('versions', function () {
     .pipe(app.dest('_gh_pages/v'));
 });
 
-app.task('default', ['versions']);
+// load common resources
+app.task('load', function (done) {
+  app.data(plasma.load(['src/data/*.{json,yml}', 'package.json']))
+  app.helpers(require('./src/extensions/helpers/helpers.js'));
+  console.log(app._.helpers);
+  // app.helpers(['src/extensions/*.js', 'helper-prettify']);
+  app.partials.load(['templates/includes/**/*.{hbs,md}']);
+  app.layouts.load(['templates/layouts/**/*.hbs']);
+  done();
+});
+
+app.task('docs', function () {
+  return app.src('templates/pages/*.hbs')
+    .pipe(app.renderFile())
+    .pipe(extname())
+    .pipe(app.dest('_gh_pages'));
+});
+
+app.task('default', ['versions', 'load', 'docs']);
 
 module.exports = app;
---
+++ src/extensions/helpers/helpers.js
@@ -0,0 +1,112 @@
+// These are some really brittle experimental helpers. I'm just having some fun
+// playing around for our docs, but I strongly recommend you not use these on
+// your own projects. - Jon
+
+var Handlebars = require('handlebars');
+var glob = require('globby');
+var fs = require('fs');
+
+var helpers = module.exports = {};
+
+var urlNormalize = function(filepath) {
+  return filepath.replace(/\\/g, "/");
+};
+
+var toString = function(val) {
+  if (val == null) {
+    return "";
+  } else {
+    return val.toString();
+  }
+};
+
+var isUndefined = function(value) {
+  return value === 'undefined' || toString.call(value) === '[object Function]' || (value.hash != null);
+};
+
+var read = function (fp) {
+  return fs.readFileSync(fp, 'utf8');
+}
+
+var globFiles = function(src) {
+  var content = glob.sync(src)
+    .map(read)
+    .join('\n');
+  return content;
+};
+
+
+// helpers['readYFM'] = function(src) {
+//   var props = assemble.data.readYFM(src, {fromFile: true});
+//   var str = "\n```json\n" + (util.inspect(props, true, null)) + "\n```\n";
+//   return new Handlebars.SafeString(str);
+// };
+
+/*
+ * This helper is used to generate links for Assemble's documentation
+ * The helper simply builds a list of pages in the given directory.
+ */
+helpers['generate-links'] = function(str, dir) {
+  if(isUndefined(dir)) {
+    dir = '';
+  } else {
+    dir = dir;
+  }
+  var content = glob.sync(str).map(function(link) {
+    var page = path.basename(link).replace(/\.md\.hbs/g, '');
+    return '[' + page.toLowerCase().replace(/\s/g, '-').replace(/\./g, '-') + ']: ' + dir + page + '.html' ;
+  });
+  return new Handlebars.SafeString(content.join('\n'));
+};
+
+helpers['links-docs'] = function(str) {
+  var content = glob.sync(str).map(function(link) {
+    var page = path.basename(link).replace(/\.hbs/g, '');
+    var docs = '';
+    if(options.docs) {
+      var docs = 'http://assemble.io/docs/';
+    }
+    return '[' + page + ']: ' + docs + page + '.html' ;
+  });
+  return new Handlebars.SafeString(content.join('\n'));
+};
+
+// helpers['links-gh-json'] = function(str, dir) {
+//   var links = {};
+//   var content = glob.sync(str).map(function(link) {
+//     var page = path.basename(link).replace(/\.hbs/g, '');
+//     return '"' + page.toLowerCase() + '": "[' + page + ']: ' + page + '.html"' ;
+//   });
+//   result = '{' + content + '}';
+//   if(isUndefined(dir)) {
+//     dir = 'src/data/gh-pages-links.json';
+//   } else {
+//     dir = dir;
+//   }
+//   // result = ;
+//   result = JSON.stringify(JSON.parse(result), null, 2);
+//   grunt.file.write(dir, result);
+// };
+
+helpers['links-docs'] = function(str) {
+  var content = glob.sync(str).map(function(link) {
+    var file = path.basename(link).replace(/\.hbs/g, '');
+    return '[' + file + ']: http://assemble.io/docs/' + file + '.html' ;
+  });
+  return new Handlebars.SafeString(content.join('\n'));
+};
+
+// helpers['links-docs-json'] = function(str, dir) {
+//   var links = {};
+//   var content = glob.sync(str).map(function(link) {
+//     var page = path.basename(link).replace(/\.hbs/g, '');
+//     return '"' + page.toLowerCase().replace(/\s/g, '').replace(/\./g, '') + '": "[' + page + ']: http://assemble.io/docs/' + page + '.html"' ;
+//   });
+//   result = '{' + content + '}';
+//   if(isUndefined(dir)) {
+//     dir = 'src/data/docs-links.json';
+//   } else {
+//     dir = dir;
+//   }
+//   grunt.file.write(dir, JSON.stringify(JSON.parse(result), null, 2));
+// };
