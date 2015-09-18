58da6457187edb39667175b5299468bddc25813a
Brian Woodward <brian.woodward@gmail.com>
2015-09-18 16:23:55 -0400

    excluding doc versions from being archived

--- assemblefile.js
+++ assemblefile.js
@@ -7,7 +7,7 @@ app.option('archive.version', '0.4.0');
 
 // archive a _gh_pages folder to the specific version folder and build a manifest of archived files
 var archive = require('./src/plugins/archive')(app);
-app.task('archive', archive('_gh_pages/**/*', 'versions'));
+app.task('archive', archive(['_gh_pages/**/*', '!_gh_pages/docs/v/**/*', '!_gh_pages/docs/v'], 'versions'));
 
 
 // copy archived versions to the docs/v folder
