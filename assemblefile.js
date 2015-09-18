190eb9c8cb94355255ca2002b09744250d8d12a9
Brian Woodward <brian.woodward@gmail.com>
2015-09-18 15:54:38 -0400

    comments

--- assemblefile.js
+++ assemblefile.js
@@ -3,9 +3,12 @@
 var assemble = require('assemble');
 
 var app = assemble();
+
+// manifest plugin configuration
 app.option('manifest.version', '0.6.0');
 app.option('manifest.app-name', 'assemble');
 
+// manifest plugin that will generate a new manifest file if version minor bump
 var manifest = require('./src/plugins/manifest')(app);
 app.task('manifest', manifest('_gh_pages/**/*', 'versions'));
 
