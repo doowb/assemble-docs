b8478a8cc69733602891d44b1a5f7cc15a7dd4e0
Brian Woodward <brian.woodward@gmail.com>
2015-09-18 16:27:46 -0400

    moving versions folder (docs/v) to root (v) since it contains more than docs

--- assemblefile.js
+++ assemblefile.js
@@ -7,13 +7,13 @@ app.option('archive.version', '0.4.0');
 
 // archive a _gh_pages folder to the specific version folder and build a manifest of archived files
 var archive = require('./src/plugins/archive')(app);
-app.task('archive', archive(['_gh_pages/**/*', '!_gh_pages/docs/v/**/*', '!_gh_pages/docs/v'], 'versions'));
+app.task('archive', archive(['_gh_pages/**/*', '!_gh_pages/v/**/*', '!_gh_pages/v'], 'versions'));
 
 
 // copy archived versions to the docs/v folder
 app.task('versions', function () {
   return app.src('versions/**/*')
-    .pipe(app.dest('_gh_pages/docs/v'));
+    .pipe(app.dest('_gh_pages/v'));
 });
 
 app.task('default', ['versions']);
